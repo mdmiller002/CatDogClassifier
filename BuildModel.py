c0d0c5aa1e76422586a5fc4f0a2b0fbe26c8f681
Matthew Miller <mdmiller002@outlook.com>
2018-10-25 09:58:26 -0400

    Removed unnecessary dropout

--- BuildModel.py
+++ BuildModel.py
@@ -51,17 +51,14 @@ def BuildModel(epochs, model=None):
         classifier.add(layers.Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=inputShape))
         classifier.add(layers.Conv2D(32, (3, 3), activation='relu'))
         classifier.add(layers.MaxPooling2D(pool_size=(2, 2)))
-        classifier.add(layers.Dropout(0.25))
 
         classifier.add(layers.Conv2D(64, (3, 3), padding='same', activation='relu'))
         classifier.add(layers.Conv2D(64, (3, 3), activation='relu'))
         classifier.add(layers.MaxPooling2D(pool_size=(2, 2)))
-        classifier.add(layers.Dropout(0.25))
 
         classifier.add(layers.Conv2D(64, (3, 3), padding='same', activation='relu'))
         classifier.add(layers.Conv2D(64, (3, 3), activation='relu'))
         classifier.add(layers.MaxPooling2D(pool_size=(2, 2)))
-        classifier.add(layers.Dropout(0.25))
 
         classifier.add(layers.Flatten())
 
@@ -69,7 +66,7 @@ def BuildModel(epochs, model=None):
         classifier.add(layers.Dropout(0.5))
         classifier.add(layers.Dense(1, activation='sigmoid'))
 
-        classifier.compile(optimizer='rmsprop',
+        classifier.compile(optimizer='adam',
                            loss='binary_crossentropy',
                            metrics=['accuracy'])
 
