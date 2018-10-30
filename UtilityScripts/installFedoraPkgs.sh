882cdbb4bce0ab9e5c799988601ef852f544a740
Matthew Miller <mdmiller002@outlook.com>
2018-10-30 13:10:38 -0400

    Added installation scripts for AWS servers

---
+++ UtilityScripts/installFedoraPkgs.sh
@@ -0,0 +1,9 @@
+# To use this script, type sh ./installFedoraPkgs.sh
+
+sudo yum install python36
+sudo yum install git
+sudo python3 -m pip install tensorflow
+sudo python3 -m pip install keras
+sudo python3 -m pip install matplotlib
+sudo python3 -m pip install pillow
+git clone https://github.com/mdmiller002/CatDogClassifier.git
---
+++ UtilityScripts/installUbuntuPkgs.sh
@@ -0,0 +1,9 @@
+# To use this script, type sh ./installFedoraPkgs.sh
+
+sudo apt-get install python3.6
+sudo apt-get install git
+sudo python3 -m pip install tensorflow
+sudo python3 -m pip install keras
+sudo python3 -m pip install matplotlib
+sudo python3 -m pip install pillow
+git clone https://github.com/mdmiller002/CatDogClassifier.git
